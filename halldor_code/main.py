1b9a1e23ef255f9372631e305db7c861631d8cdb
aksha <akshayavhad89@gmail.com>
2023-02-25 08:34:08 +0100

    task release interface

--- halldor_code/main.py
+++ halldor_code/main.py
@@ -1,101 +1,11 @@
 import sys
 import threading
-from dataclasses import dataclass, replace
+from Task_Planner import Task_PG, order
 from halldor_code.Robot_agent import Transfer_robot, Workstation_robot
 from halldor_code.client_2 import start_opcua
 from halldor_code.task_allocation import Task_Allocation
 
-### sink placement 12###
-## Source = 0 , Cell type = 1 , Server type =2 , Sink = 3######
 
-order = {
-        "Name": "Test",
-        "PV": 10,
-        "sequence": [[11, 1, 3, 5, 6, 8, 9, 12],
-                      [11, 2, 4, 5, 7, 8, 10, 12],
-                      [11, 3, 5, 6, 8, 9, 10, 12],
-                      [11, 5, 7, 8, 9, 10, 12],
-                      [11, 1, 4, 5, 7, 8, 9, 10, 12],
-                      [11, 2, 5, 6, 8, 10, 12],
-                      [11, 3, 6, 8, 2, 4, 3, 12],
-                      [11, 4, 5, 6, 8, 10, 7, 12],
-                      [11, 3, 4, 6, 1, 8, 9, 10, 12],
-                      [11, 2, 4, 6, 8, 5, 7, 9, 10, 12]
-                      ],
-
-        "PI"    : [1,1,1,1,1,1,1,1,1,1],
-        "Wk_type" : [0,1,1,1,1,1,1,1,1,1,1,3],
-        "Process_times": [0, 20, 30, 40, 50, 20, 40, 80, 70, 30, 60, 0]
-
-}
-
-#print(order["sequence"])
-## status : Pending / running / Performed
-## Allocation : 0 (not alloted)
-@dataclass(frozen=True)
-class task:
-    id : int
-    type: int
-    command: []
-    product: int
-    allocation: bool
-    status: str
-    robot: int
-
-    def __getitem__(self,id):
-        return getattr(self,id)
-
-    def assign(self, robot):
-        object.__setattr__(self, 'allocation', True)
-        object.__setattr__(self, 'robot', robot)
-
-    def deassign(self, robot):
-        object.__setattr__(self, 'allocation', False)
-        object.__setattr__(self, 'robot', robot)
-
-    def cstatus(self, status):
-        object.__setattr__(self, 'status', status)
-
-
-
-class Task_PG:
-
-    def __init__(self, order):
-        self.order = order
-
-    def task_list(self):
-        task_list = []
-        tl = []
-        n = 0
-        task_dict_list = []
-        task_enum = 0
-        t2 = []
-        seq = self.order["sequence"]
-        for i in range(len(seq)):
-            tl = []
-            #task_list.append([0, i+1])
-            for j in range(len(seq[i]) - 1):
-                # print(graph[i][j], graph[i][j+1])
-                tasks = [seq[i][j], seq[i][j + 1]]
-                tl.append(tasks)
-            task_list.append(tl)
-
-        for t1 in enumerate(task_list):
-              for t2 in t1[1]:
-                n = n+1
-                #print(n,t1[0],t2[0], t2[1])
-                if t2[0] == 11 or t2[1] ==11:
-                    type = 1
-                elif t2[0] == 12 or t2[1] ==12:
-                    type = 4
-                else:
-                    type = 2
-                task_node = task(n, type, t2, t1[0]+1,False, "Pending",999)
-                task_dict_list.append(task_node)
-
-        return task_dict_list
-
-    #def graph(self):
 
 
 ### instantiate order and generation of task list to that order
@@ -104,7 +14,7 @@ task_list = test_order.task_list()
 
 print(task_list)
 
-#### initialize OPCUA client
+#### initialize OPCUA client to communicate to Visual Components ###################
 
 data_opcua = {
             "brand": "Ford",
@@ -118,7 +28,10 @@ data_opcua = {
 # x = threading.Thread(target=start_opcua, args=(data_opcua,))
 # x.start()
 
-########Initializar Workstation robots
+
+
+#########Initialization of Workstation robots###############################
+
 W_robot = []
 for i, (type,pt) in enumerate(zip(order["Wk_type"], order["Process_times"])):
     if type==1 or type==2:
@@ -127,14 +40,17 @@ for i, (type,pt) in enumerate(zip(order["Wk_type"], order["Process_times"])):
         W_robot.append(wr)
 
 
-#### Initializate Carrier robots
+########## Initialization of Carrier robots######################################################
 T_robot = []
 for i , R in enumerate(data_opcua["rob_busy"]):
     #print(i+1, R)
     robot = Transfer_robot(i+1, task_list, data_opcua)
     T_robot.append(robot)
 
-### perform task allocation#####
+
+
+###################### perform task allocation####################################
+
 ## Initialize Task Allocator
 Greedy_Allocator = Task_Allocation(task_list, data_opcua)
 
@@ -146,7 +62,11 @@ Greedy_Allocator.bid_counter(T_robot)
 
 
 
-### Test function######
+
+
+
+
+############################## Test debug function###########################################
 #broadcast_bid(task_list)
 
 # for t in task_list:
