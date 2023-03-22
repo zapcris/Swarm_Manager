7ce4946de854816eddf29146e9dc8c8dbefc12de
Akshay Avhad <akshayavhad89@gmail.com>
2023-03-22 17:58:57 +0100

    Update: New task waiting thread added for unalloted tasks

--- Test_implementation/try_code.py
+++ Test_implementation/try_code.py
@@ -1,18 +1,45 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
+import asyncio
+from threading import Thread
+from time import sleep
+
+q1 = asyncio.Queue()
 
 
-def multiple(a):
-    if a ==1 :
-        c = 100
-        return c
-    if a  ==2 :
-        c = 200
-        return c
+
+async def queue(q1):
+    while True:
+        try:
+            data, prod = q1.get_nowait()
+            print("Retrieved value", data, prod)
+            if data !=0 :
+                print("Data alloted")
+            elif data ==0:
+                await asyncio.sleep(5)
+                print("Data queued back again")
+                prod +=100
+                q1.put_nowait((data, prod))
+
+
+        except:
+            pass
+
+def run_queue(q1):
+    asyncio.run(queue(q1))
+
+
+task_waiting_thread = Thread(target=run_queue, daemon=True, args=(q1, ))
+task_waiting_thread.start()
+
+count = 0
+
+while True:
+    a = 0
+
+    sleep(4)
+    if a == 0 and count ==0 :
+        q1.put_nowait((a,100))
+        print("Data entered")
+        count += 1
+
     else:
-        print("Nothing")
-        return None
-
-
-print(multiple(3))
+        pass
